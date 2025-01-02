add5fc8b1e89e6ddc3d0cc4694c206d206cce7bb
vipabh2 <a8919832@googlemail.com>
2025-01-03 01:19:50 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,11 +8,11 @@ bot_token = os.getenv('BOT_TOKEN')
 client = TelegramClient('new_bot_session', api_id, api_hash).start(bot_token=bot_token)
 
 abh = [
-    # "ها",
-    # "شرايد",
-    # "تفظل",
-    # "قُل",
-    # "😶",
+    "ها",
+    "شرايد",
+    "تفظل",
+    "قُل",
+    "😶",
     "https://t.me/VIPABH/1214"
 ]
 
