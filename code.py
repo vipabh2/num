72f43a70767704ee65ad0b4515cc6db64a1791bd
vipabh2 <a8919832@googlemail.com>
2025-01-03 01:25:11 +0300

    Update code.py

--- code.py
+++ code.py
@@ -27,5 +27,55 @@ async def reply(event):
         await event.reply(file=vipabh)
     else:
         await event.reply(vipabh)
+        
+url = "https://ar.wikipedia.org/w/api.php"
+searching_state = {}
+@client.on(events.NewMessage(func=lambda e: e.text and e.text.strip().lower().startswith('ابحث عن')))
+async def cut(event):
+    search_term = event.text.strip().lower().replace('ابحث عن', '').strip()
+
+    if not search_term:
+        await event.reply("من فضلك أدخل الكلمة التي تريد البحث عنها بعد 'ابحث عن'.")
+        return
+    params = {
+        "action": "query",
+        "list": "search",
+        "srsearch": search_term,
+        "format": "json",
+        "utf8": 1,
+        "srlimit": 3  
+    }
+    response = requests.get(url, params=params)   
+    if response.status_code == 200:
+        data = response.json()
+        if 'query' in data and 'search' in data['query']:
+            if not data['query']['search']:
+                await event.reply("لا يوجد نتائج لهذا البحث.")
+            else:
+                found_exact_match = False
+                for result in data['query']['search']:
+                    if result['title'].lower() == search_term:
+                        found_exact_match = True
+                        snippet = BeautifulSoup(result['snippet'], "html.parser").get_text()
+                        snippet = snippet[:1000] + "..." if len(snippet) > 1000 else snippet  # 1000 حرف هنا
+                        article_url = f"https://ar.wikipedia.org/wiki/{result['title']}"
+                        
+                        await event.reply(f"عنوان المقال: \n {result['title']}\n"
+                                          f"المقال: \n {snippet}\n"
+                                          f"{'-' * 40}")
+                
+                if not found_exact_match:
+                    await event.reply(
+                        f"لا يوجد نتائج تطابق {search_term} \n لكن جرب `ابحث عام {search_term}`",
+                        parse_mode="Markdown"
+                                     )
+        else:
+            await event.reply("حدث خطأ في استجابة API.")
+    else:
+        await event.reply(f"حدث خطأ في الاتصال بـ Wikipedia. حاول مرة أخرى لاحقًا.")
+
+
+
+
 
 client.run_until_disconnected()
