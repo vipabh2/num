d1b72f93d1daf702494e3448c45a7680b9bca445
vipabh2 <a8919832@googlemail.com>
2025-01-03 01:02:00 +0300

    Update code.py

--- code.py
+++ code.py
@@ -11,33 +11,8 @@ from models import add_or_update_user, add_point_to_winner, get_user_score
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
-client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
+client = TelegramClient('new_bot_session', api_id, api_hash).start(bot_token=bot_token)
 
-
-user_id_to_delete = 1910015590
-bot_id_to_delete = 793977288
-
-client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
-
-@client.on(events.NewMessage)
-async def delete_user_messages(event):
-    if event.sender_id == user_id_to_delete and "/send" in event.raw_text:
-        try:
-            await event.delete()
-            print(f"تم حذف الرسالة من المستخدم {user_id_to_delete} بنجاح.")
-        except Exception as e:
-            print(f"حدث خطأ أثناء محاولة حذف الرسالة: {e}")
-
-@client.on(events.NewMessage)
-async def delete_other_bots_messages(event):
-    if event.sender_id != client.me.id:
-        try:
-            sender = await event.get_sender()
-            if sender.bot: 
-                await event.delete()
-                print(f"تم حذف رسالة البوت {event.sender_id}.")
-        except Exception as e:
-            print(f"حدث خطأ أثناء محاولة حذف رسالة البوت: {e}")
 
 print("البوت قيد التشغيل...")
 while True:
