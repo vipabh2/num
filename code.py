b47c6a76aa1b313eb508ca61716c5a739900d63c
vipabh2 <a8919832@googlemail.com>
2025-01-03 13:41:01 +0300

    Update code.py

--- code.py
+++ code.py
@@ -311,7 +311,7 @@ async def send_basim(call):
     await send_audio_from_list(call, furl)
     await call.edit(buttons=None)
 ###########################################
-# video_urls = [f"t.me/iuabh/{i}" for i in range(242, 256)]
+video_urls = [f"t.me/iuabh/{i}" for i in range(242, 256)]
 @client.on(events.NewMessage(func=lambda message: message.text in ['ميم', 'ميمز']))
 async def send_random_file(event):
     # await asyncio.sleep(2)
@@ -320,15 +320,15 @@ async def send_random_file(event):
     url = f"t.me/iuabh/{rl}"
 
     try:
-        # if url in video_urls:
-        #     await event.reply(
-        #         video=url,
-        #         caption="😎يسعد مسائك",
-        #         reply_to=event.message.id
-        #     )
-        # else:
+        if url in video_urls:
             await event.reply(
-                media=InputMediaPhoto(url),
+                video=url,
+                caption="😎يسعد مسائك",
+                reply_to=event.message.id
+            )
+        else:
+            await event.reply(
+                media=url,
                 caption="😎يسعد مسائك",
                 reply_to=event.message.id
             )
