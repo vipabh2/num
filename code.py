679cd21778afb2edd121064d5f7598cabe18631c
vipabh2 <a8919832@googlemail.com>
2025-01-03 13:29:29 +0300

    Update code.py

--- code.py
+++ code.py
@@ -305,12 +305,30 @@ async def send_basim(call):
 async def send_basim(call):
     await send_audio_from_list(call, furl)
     await call.edit(buttons=None)
-
-
-
-
-
 ###########################################
+video_urls = [f"t.me/iuabh/{i}" for i in range(242, 256)]
+@client.on(events.NewMessage(func=lambda message: message.text in ['ميم', 'ميمز']))
+async def send_random_file(event):
+    await asyncio.sleep(2)
+    rl = random.randint(2, 255)
+    url = f"t.me/iuabh/{rl}"
+    try:
+        if url in video_urls:
+            await event.reply(
+                video=url,
+                caption="😎يسعد مسائك",
+                reply_to=event.message.id
+            )
+        else:
+            await event.reply(
+                media=InputMediaPhoto(url),
+                caption="😎يسعد مسائك",
+                reply_to=event.message.id
+            )
+    except Exception as e:
+        await event.reply("حدث خطأ أثناء إرسال الملف.")
+
+
 
 
 client.run_until_disconnected()
