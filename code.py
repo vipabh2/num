fbdc4876f250422aed59527c2c3394a6473c3e17
vipabh2 <a8919832@googlemail.com>
2025-01-03 12:31:23 +0300

    Update code.py

--- code.py
+++ code.py
@@ -10,7 +10,7 @@ import os
 api_id = os.getenv('API_ID')      
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
-client = TelegramClient('new_bot_session', api_id, api_hash).start(bot_token=bot_token)
+client = TelegramClient('n', api_id, api_hash).start(bot_token=bot_token)
 #######################################################################################
 abh = [
     "ها",
