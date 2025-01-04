7c6f74ef3bd35cd1f5a519d0323e1be5e6337323
vipabh2 <a8919832@googlemail.com>
2025-01-05 02:20:19 +0300

    Update code.py

--- code.py
+++ code.py
@@ -17,50 +17,50 @@ client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 user_ban_times = {}
 
 # حدث لكشف التقييد
-@client.on(events.UserUpdate)
-async def user_update_handler(event):
-    try:
-        if event.user_id and event.is_banned:
-            user_id = event.user_id
-            ban_time = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
-            print(ban_time)
+# @client.on(events.UserUpdate)
+# async def user_update_handler(event):
+#     try:
+#         if event.user_id and event.is_banned:
+#             user_id = event.user_id
+#             ban_time = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
+#             print(ban_time)
 
-            user_ban_times[user_id] = ban_time  # حفظ وقت الحظر
-            print(user_ban_times)
+#             user_ban_times[user_id] = ban_time  # حفظ وقت الحظر
+#             print(user_ban_times)
 
-            if hasattr(event, 'chat_id'):
-                group_username = event.chat_id  # إذا كان event يحتوي على معرّف المجموعة
-                await client.send_message(group_username, f"تم تقييد المستخدم {user_id} في {ban_time}.")
-    except Exception as e:
-        print(f"Error occurred while updating user ban: {e}")
+#             if hasattr(event, 'chat_id'):
+#                 group_username = event.chat_id  # إذا كان event يحتوي على معرّف المجموعة
+#                 await client.send_message(group_username, f"تم تقييد المستخدم {user_id} في {ban_time}.")
+#     except Exception as e:
+#         print(f"Error occurred while updating user ban: {e}")
 
 
-async def get_users_without_write_permission(event):
-    try:
-        group_username = event.chat_id
+# async def get_users_without_write_permission(event):
+#     try:
+#         group_username = event.chat_id
 
-        participants = await client(GetParticipantsRequest(
-            channel=group_username,
-            filter=ChannelParticipantsBanned(q=""),
-            offset=0,
-            limit=100,
-            hash=0
-        ))
+#         participants = await client(GetParticipantsRequest(
+#             channel=group_username,
+#             filter=ChannelParticipantsBanned(q=""),
+#             offset=0,
+#             limit=100,
+#             hash=0
+#         ))
 
-        if not participants.users:
-            await event.reply("لا يوجد مستخدمون محظورون في هذه المجموعة.")
-            return
+#         if not participants.users:
+#             await event.reply("لا يوجد مستخدمون محظورون في هذه المجموعة.")
+#             return
 
-        for user in participants.users:
-            mention = f"[@{user.username}](https://t.me/@{user.username})" if user.username else f"[{user.first_name}](tg://user?id={user.id})"
-            ban_info = user_ban_times.get(user.id, {"ban_time": "غير معروف", "first_name": "غير معروف"})
-            ban_time = ban_info["ban_time"]
-            # print(ban_info)
+#         for user in participants.users:
+#             mention = f"[@{user.username}](https://t.me/@{user.username})" if user.username else f"[{user.first_name}](tg://user?id={user.id})"
+#             ban_info = user_ban_times.get(user.id, {"ban_time": "غير معروف", "first_name": "غير معروف"})
+#             ban_time = ban_info["ban_time"]
+#             # print(ban_info)
 
-            first_name = ban_info["first_name"]
-            await event.reply(f"User: {first_name} - {mention}\nBanned Time: {ban_time}", parse_mode="md")
-    except Exception as e:
-        print(f"Error occurred while getting banned users: {e}")
+#             first_name = ban_info["first_name"]
+#             await event.reply(f"User: {first_name} - {mention}\nBanned Time: {ban_time}", parse_mode="md")
+#     except Exception as e:
+#         print(f"Error occurred while getting banned users: {e}")
 
         
 # تنفيذ أمر "/get_banned"
