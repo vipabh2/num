ea5fa914e11f90a830fe2004b3cf5dbccb6ad740
vipabh2 <a8919832@googlemail.com>
2025-01-06 00:46:45 +0300

    Update code.py

--- code.py
+++ code.py
@@ -17,72 +17,55 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')  
 bot_token = os.getenv('BOT_TOKEN') 
 Client = TelegramClient('n', api_id, api_hash).start(bot_token=bot_token)
-##########################################################################from telethon import events
+##########################################################################
 
-@Client.on(events.NewMessage(pattern=r"ميمز (\S+) (.+)"))
+@bot.on(events.NewMessage(pattern=r"ميمز (\S+) (.+)"))
 async def Hussein(event):
     url = event.pattern_match.group(1)
     lMl10l = event.pattern_match.group(2)
-    add_link(lMl10l, url)
-    # await event.edit(f"**᯽︙ تم اضافة البصمة {lMl10l} بنجاح ✓ **")
+    add_link(lMl10l, url)  
     ABH = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
     ABH = Get(ABH)
     try:
-        await event.Client(ABH)
+        await event.client.send_message(event.chat_id, "تم إضافة البصمة بنجاح")
     except BaseException:
         pass
-@Client.on(events.NewMessage(pattern=r"\?(.*)"))
+
+@bot.on(events.NewMessage(pattern=r"\?(.*)"))
 async def Hussein(event):
     lMl10l = event.pattern_match.group(1)
-    Client = await reply_id(event)
     url = get_link(lMl10l)
     if url:
-        await event.Client.send_file(event.chat_id, url, parse_mode="html", reply_to=Client)
+        await event.client.send_file(event.chat_id, url, parse_mode="html")
         await event.delete()
-        ABH = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-        ABH = Get(ABH)
-        try:
-            await event.Client(ABH)
-        except BaseException:
-            pass
 
-@Client.on(events.NewMessage(pattern="ازالة(?:\s|$)([\s\S]*)"))
+@bot.on(events.NewMessage(pattern="ازالة(?:\s|$)([\s\S]*)"))
 async def delete_alClient(event):
     lMl10l = event.pattern_match.group(1)
     delete_link(lMl10l)
-    # await event.edit(f"**᯽︙ تم حذف البصمة '{lMl10l}' بنجاح ✓**")
     ABH = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
     ABH = Get(ABH)
     try:
-        await event.Client(ABH)
+        await event.client.send_message(event.chat_id, f"تم حذف البصمة '{lMl10l}' بنجاح")
     except BaseException:
         pass
-@Client.on(events.NewMessage(pattern="قائمة الميمز"))
+
+@bot.on(events.NewMessage(pattern="قائمة الميمز"))
 async def list_alClient(event):
     links = SESSION.query(AljokerLink).all()
     if links:
-        message = "**᯽︙ قائمة تخزين اوامر الميمز:**\n"
+        message = "**قائمة الميمز:**\n"
         for link in links:
-            message += f"- البصمة : .`{link.key}`\n"
+            message += f"- {link.key}: {link.url}\n"
     else:
-        message = "**᯽︙ لاتوجد بصمات ميمز مخزونة حتى الآن**"
-    # await event.edit(message)
-    ABH = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-    ABH = Get(ABH)
-    try:
-        await event.Client(ABH)
-    except BaseException:
-        pass
-@Client.on(events.NewMessage(pattern="ازالة_البصمات"))
+        message = "**لاتوجد بصمات ميمز مخزونة حتى الآن**"
+    await event.client.send_message(event.chat_id, message)
+
+@bot.on(events.NewMessage(pattern="ازالة_البصمات"))
 async def delete_all_alClient(event):
     SESSION.query(AljokerLink).delete()
-    # await event.edit("**᯽︙ تم حذف جميع بصمات الميمز من القائمة **")
-    ABH = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-    ABH = Get(ABH)
-    try:
-        await event.Client(ABH)
-    except BaseException:
-        pass
+    await event.client.send_message(event.chat_id, "تم حذف جميع بصمات الميمز")
+
 
 if __name__ == "__main__":
     while True:
