29b3d71f407c5535459a96f08c40748627a096f0
vipabh2 <a8919832@googlemail.com>
2025-01-06 02:54:14 +0300

    Update code.py

--- code.py
+++ code.py
@@ -20,53 +20,6 @@ bot_token = os.getenv('BOT_TOKEN')
 Client = TelegramClient('n', api_id, api_hash).start(bot_token=bot_token)
 ##########################################################################
 
-@Client.on(events.NewMessage(pattern=r"ميمز (\S+) (.+)"))
-async def Hussein(event):
-    url = event.pattern_match.group(1)
-    lMl10l = event.pattern_match.group(2)
-    add_link(lMl10l, url)  
-    ABH = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-    ABH = Get(ABH)
-    try:
-        await event.client.send_message(event.chat_id, "تم إضافة البصمة بنجاح")
-    except BaseException:
-        pass
-
-@Client.on(events.NewMessage(pattern=r"\?(.*)"))
-async def Hussein(event):
-    lMl10l = event.pattern_match.group(1)
-    url = get_link(lMl10l)
-    if url:
-        await event.client.send_file(event.chat_id, url, parse_mode="html")
-        await event.delete()
-
-@Client.on(events.NewMessage(pattern="ازالة(?:\s|$)([\s\S]*)"))
-async def delete_alClient(event):
-    lMl10l = event.pattern_match.group(1)
-    delete_link(lMl10l)
-    ABH = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-    ABH = Get(ABH)
-    try:
-        await event.client.send_message(event.chat_id, f"تم حذف البصمة '{lMl10l}' بنجاح")
-    except BaseException:
-        pass
-
-@Client.on(events.NewMessage(pattern="قائمة الميمز"))
-async def list_alClient(event):
-    links = SESSION.query(AljokerLink).all()
-    if links:
-        message = "**قائمة الميمز:**\n"
-        for link in links:
-            message += f"- {link.key}: {link.url}\n"
-    else:
-        message = "**لاتوجد بصمات ميمز مخزونة حتى الآن**"
-    await event.client.send_message(event.chat_id, message)
-
-@Client.on(events.NewMessage(pattern="ازالة_البصمات"))
-async def delete_all_alClient(event):
-    SESSION.query(AljokerLink).delete()
-    await event.client.send_message(event.chat_id, "تم حذف جميع بصمات الميمز")
-
 
 if __name__ == "__main__":
     while True:
